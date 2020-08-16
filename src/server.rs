4684906ed822d32249152dee83826734d69b40fd
Christoffer Lantz <christoffer.lantz@androidbutiken.net>
2020-08-16 16:54:24 +0200

    Use trait AsyncToSocketAddrs in Server::new

--- src/server.rs
+++ src/server.rs
@@ -1,14 +1,15 @@
 use {
     crate::AsyncResult,
-    async_net::{Incoming, TcpListener},
+    async_net::{Incoming, TcpListener, AsyncToSocketAddrs},
 };
+
 /// A [`TcpListener`] handling incoming [`TcpStream`](`async_net::TcpStream`)
 pub struct Server {
     connection: TcpListener,
 }
 impl<'a> Server {
     /// Opens up a [`TcpListener`] waiting for incoming connections on a given address
-    pub async fn new(addr: &str) -> AsyncResult<Server> {
+    pub async fn new<A: AsyncToSocketAddrs>(addr: A) -> AsyncResult<Server> {
         let listener = TcpListener::bind(addr).await?;
         Ok(Server {
             connection: listener,
